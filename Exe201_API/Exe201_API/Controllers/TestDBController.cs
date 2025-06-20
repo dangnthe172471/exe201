991ebfbb38186f1014e0023046d3d570fa93c4f0
dangnthe172471 <dangnthe172471@fpt.edu.vn>
2025-06-20 21:29:23 +0700

    update BE

---
+++ Exe201_API/Exe201_API/Controllers/TestDBController.cs
@@ -0,0 +1,20 @@
+﻿using Exe201_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exe201_API.Controllers
+{
+    public class TestDBController : Controller
+    {
+        private readonly CareUContext _context;
+        public TestDBController(CareUContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("getUser")]
+        public IActionResult getUser()
+        {
+            var result = _context.Users.ToList();
+            return Ok(result);
+        }
+    }
+}
---
+++ Exe201_API/Exe201_API/Program.cs
@@ -0,0 +1,43 @@
+using Exe201_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CareUContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+builder.Services.AddCors();
+
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
